4e8c7dc2b8ce3c67c022202eddb77e6c6c09f60b
spezi77 <spezi7713@gmx.net>
2021-02-26 17:21:24 +0100

    sepolicy: Add LiveDisplay rules from Lineage.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ exynos/sepolicy.mk
@@ -0,0 +1,23 @@
+#
+# This policy configuration will be used by all exynos products
+# that inherit from PixelDust
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/pixeldust/sepolicy/exynos/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/pixeldust/sepolicy/exynos/dynamic \
+    device/pixeldust/sepolicy/exynos/system
+else
+BOARD_SEPOLICY_DIRS += \
+    device/pixeldust/sepolicy/exynos/dynamic \
+    device/pixeldust/sepolicy/exynos/vendor
+endif
